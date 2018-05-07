a12fee027abdc72c73ce7c3a3d4231e9fb73d5cd
Katy Deng <deng2017.xq@gmail.com>
2018-05-06 22:05:44 -0700

    compelted the react-intro proctopypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+function luckyNumber(){
+    const randomNumber = ((Math.random()* 1000) + 1 ) >> 0;
+    console.log(randomNumber);
+    return randomNumber;
+}
+const user = {
+    name: 'katy', 
+    luckyNumber: luckyNumber()
+};
+function greeting(user){
+    return <div className = 'container'> 
+                <h1>Welcome {user.name}</h1>
+                <h2 className = 'text-muted'>This is your {user.luckyNumber}</h2>
 
+          </div>
+}
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
