bebc0e0f3871c99a9f7003fd74d8af5755dd8087
Katy Deng <deng2017.xq@gmail.com>
2018-05-06 22:31:57 -0700

    compeleted func. components challenge

---
+++ src/components/greet.js
@@ -0,0 +1,14 @@
+// this is a greet component
+
+import React from 'react';
+
+
+
+const Greeting = (props)=>{
+  return (
+    <h1> Hello, {props.name}</h1>
+  )
+  
+};
+
+export default Greeting;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Greeting from './components/greet';
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    <Greeting name ="katy"/>,
     document.getElementById('root')
 );
